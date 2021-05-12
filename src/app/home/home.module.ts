d45ad02ea40d46f188e9147a0e6018a5936267b0
AdelTalat <adel.talat@outlook.com>
2021-05-12 07:25:26 +0200

    how we helped section

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,10 +6,11 @@ import { HomeComponent } from './home.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { SharedModule } from '../shared/shared.module';
 import { LatestNewsComponent } from './components/latest-news/latest-news.component';
+import { HowWeHelpedComponent } from './components/how-we-helped/how-we-helped.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, CarouselComponent, LatestNewsComponent],
+  declarations: [HomeComponent, CarouselComponent, LatestNewsComponent, HowWeHelpedComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
