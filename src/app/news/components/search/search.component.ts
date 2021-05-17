9eb5378758b047e4c60f34107229443d8d837447
AdelTalat <adel.talat@outlook.com>
2021-05-17 22:03:48 +0200

    responsive

--- src/app/news/components/search/search.component.ts
+++ src/app/news/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Category } from 'src/app/shared/models/category';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-search',
@@ -7,17 +8,39 @@ import { Category } from 'src/app/shared/models/category';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  @Output() filterItem = new EventEmitter<number>();
+
   categories: Category[] = [];
   selectedCategoryId;
   isAscending = true;
-  constructor() { }
+
+  @Output() filterItem = new EventEmitter<number>();
+  @Output() searchQuery = new EventEmitter<string>();
+  @Output() sortFlag = new EventEmitter<boolean>();
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.categoryService.getCategories().subscribe((categories) => {
+      this.categories = categories;
+    });
   }
 
   onFilterWithCategory() {
+    console.log(this.selectedCategoryId);
     this.filterItem.emit(this.selectedCategoryId);
+  }
+
+  onSearch(value) {
+    this.searchQuery.emit(value);
+  }
+
+  onSort() {
+    this.isAscending = !this.isAscending;
+    this.sortFlag.emit(this.isAscending);
   }
 
 }
--- src/app/news/views/news-listing/news-listing.component.ts
+++ src/app/news/views/news-listing/news-listing.component.ts
@@ -9,6 +9,8 @@ import { NewsService } from '../../services/news.service';
 })
 export class NewsListingComponent implements OnInit {
     articles: Article[];
+    filteredArticles: Article[] = [];
+
     constructor(private newsService: NewsService) { }
 
     ngOnInit() {
@@ -16,7 +18,27 @@ export class NewsListingComponent implements OnInit {
     }
 
     getAllNews() {
-        this.newsService.getAllNews().subscribe(news => this.articles = news);
+        this.newsService.getAllNews().subscribe(news => this.articles = this.filteredArticles = news);
+    }
+
+    filterWithCategory(categoryId: number) {
+      this.filteredArticles = categoryId ? this.articles.filter(article => article.sourceID === categoryId) : this.articles;
+    }
+
+    filter(query: string) {
+      this.filteredArticles =
+        query ? this.articles.filter(article => article.title.toLowerCase().includes(query.toLowerCase())) : this.articles;
+    }
+
+    sort(flag: boolean) {
+      console.log(flag);
+      this.articles.sort((a, b) => {
+        if (flag) {
+          return new Date(a.publishedAt).getTime() - new Date(b.publishedAt).getTime();
+        } else {
+          return new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime();
+        }
+      });
     }
 
 }
